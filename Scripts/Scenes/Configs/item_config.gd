38242669dbf29a554b65655e0fe517a2195af4a1
inferal <pyurik44@gmail.com>
2024-11-19 11:41:19 +0300

    2: 1. Item Resources

--- Scripts/Scenes/Configs/item_config.gd
+++ Scripts/Scenes/Configs/item_config.gd
@@ -1,7 +1,16 @@
 class_name ItemConfig
 
 enum Keys{
-	Stick, #Палка
-	Stone,  #Камень
+	Stick,
+	Stone, 
 	Plant
 }
+
+const ITEM_RESOURCE_PATHS := {
+	Keys.Stick : "res://Resources/ItemResources/stick_resource.tres",
+	Keys.Stone : "res://Resources/ItemResources/stick_resource.tres",
+	Keys.Plant : "res://Resources/ItemResources/plant_resource.tres"
+}
+
+func get_item_resource(key : Keys) -> ItemResource:
+	return load(ITEM_RESOURCE_PATHS.get(key))
