fabb7cc8d452f9bb0c205aee424dbc8b8072e372
inferal <pyurik44@gmail.com>
2024-11-21 10:49:44 +0300

    +++

--- Scripts/Scenes/Configs/bulletin_config.gd
+++ Scripts/Scenes/Configs/bulletin_config.gd
@@ -6,12 +6,9 @@ enum Keys{
 }
 
 const BULLETIN_PATHS := {
-	Keys.InteractionPrompt : "res://Bulletins/Interaction_prompt/iteraction_prompt.tscn",
+	Keys.InteractionPrompt : "res://Bulletins/iteraction_prompt.tscn",
 	Keys.CraftingMenu : "res://Bulletins/Player_menus/crafting_menu.tscn"
 }
-
-#static func get_bulletin(bulletin_key: Keys) -> PackedScene:
-	#return load(BULLETIN_PATHS.get(bulletin_key))
 
 static func get_bulletin(bulletin_key: Keys) -> Bulletin:
 	return load(BULLETIN_PATHS.get(bulletin_key)).instantiate()
